654e959af1e7b9c86d0a4b7b18b6ba0421b7d06b
Frederic Dang Tran <frederic.dangtran@orange.com>
2012-11-17 16:47:02 +0100

    added support for extension attributes for json and xml current limitation: only string are supported

--- sirocco-cimi-api-server-impl/src/main/java/org/ow2/sirocco/apis/rest/cimi/domain/CimiCommon.java
+++ sirocco-cimi-api-server-impl/src/main/java/org/ow2/sirocco/apis/rest/cimi/domain/CimiCommon.java
@@ -30,15 +30,30 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlValue;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.dom.DOMSource;
 
+import org.codehaus.jackson.annotate.JsonAnyGetter;
+import org.codehaus.jackson.annotate.JsonAnySetter;
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.ow2.sirocco.apis.rest.cimi.validator.GroupWrite;
 import org.ow2.sirocco.apis.rest.cimi.validator.constraints.NotEmptyIfNotNull;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * Class with the common attributes of multiple resources.
@@ -78,6 +93,8 @@ public class CimiCommon implements CimiDataCommon {
     @JsonProperty
     @NotEmptyIfNotNull(groups = {GroupWrite.class})
     private Map<String, String> properties;
+
+    private Map<String, Object> extensionAttributes;
 
     /**
      * {@inheritDoc}
@@ -175,6 +192,50 @@ public class CimiCommon implements CimiDataCommon {
         }
     }
 
+    @JsonAnySetter
+    public void addExtensionAttribute(final String key, final Object value) {
+        if (this.extensionAttributes == null) {
+            this.extensionAttributes = new HashMap<String, Object>();
+        }
+        this.extensionAttributes.put(key, value);
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getExtensionAttributes() {
+        return this.extensionAttributes;
+    }
+
+    public Object getExtensionAttribute(final String key) {
+        if (this.extensionAttributes != null) {
+            return this.extensionAttributes.get(key);
+        } else {
+            return null;
+        }
+    }
+
+    @XmlAnyElement
+    @JsonIgnore
+    public List<ExtensionAttribute> getXmlExtensionAttributes() {
+        List<ExtensionAttribute> xmlExtensionAttributes = new ArrayList<ExtensionAttribute>();
+        if (this.extensionAttributes != null) {
+            for (Map.Entry<String, Object> entry : this.extensionAttributes.entrySet()) {
+                xmlExtensionAttributes.add(new ExtensionAttribute(entry.getKey(), entry.getValue()));
+            }
+        }
+        return xmlExtensionAttributes;
+    }
+
+    public void setXmlExtensionAttributes(final List<ExtensionAttribute> xmlExtensionAttributes) {
+        if (xmlExtensionAttributes != null) {
+            if (this.extensionAttributes == null) {
+                this.extensionAttributes = new HashMap<String, Object>();
+            }
+            for (ExtensionAttribute attribute : xmlExtensionAttributes) {
+                this.extensionAttributes.put(attribute.name, attribute.value);
+            }
+        }
+    }
+
     /**
      * Key-value class for JAXB.
      */
@@ -185,6 +246,102 @@ public class CimiCommon implements CimiDataCommon {
 
         @XmlValue
         private String value;
+    }
+
+    @XmlJavaTypeAdapter(ExtensionAttributeAdapter.class)
+    private static class ExtensionAttribute {
+        ExtensionAttribute() {
+        }
+
+        ExtensionAttribute(final String name, final Object value) {
+            super();
+            this.name = name;
+            this.value = value;
+        }
+
+        String name;
+
+        Object value;
+    }
+
+    public static class ExtensionAttributeAdapter extends XmlAdapter<Element, ExtensionAttribute> {
+
+        private ClassLoader classLoader;
+
+        private DocumentBuilder documentBuilder;
+
+        private JAXBContext jaxbContext;
+
+        public ExtensionAttributeAdapter() {
+            this.classLoader = Thread.currentThread().getContextClassLoader();
+        }
+
+        public ExtensionAttributeAdapter(final JAXBContext jaxbContext) {
+            this();
+            this.jaxbContext = jaxbContext;
+        }
+
+        private DocumentBuilder getDocumentBuilder() throws Exception {
+            // Lazy load the DocumentBuilder as it is not used for
+            // unmarshalling.
+            if (null == this.documentBuilder) {
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                this.documentBuilder = dbf.newDocumentBuilder();
+            }
+            return this.documentBuilder;
+        }
+
+        private JAXBContext getJAXBContext(final Class<?> type) throws Exception {
+            if (null == this.jaxbContext) {
+                // A JAXBContext was not set, so create a new one based on the
+                // type.
+                return JAXBContext.newInstance(type);
+            }
+            return this.jaxbContext;
+        }
+
+        @Override
+        public Element marshal(final ExtensionAttribute attribute) throws Exception {
+            if (null == attribute) {
+                return null;
+            }
+
+            // 1. Build the JAXBElement to wrap the instance of
+            // ExtensionAttribute.
+            QName rootElement = new QName("http://schemas.dmtf.org/cimi/1", attribute.name, "");
+            Object value = attribute.value;
+            Class<?> type = value.getClass();
+            JAXBElement<?> jaxbElement = new JAXBElement(rootElement, type, value);
+
+            // 2. Marshal the JAXBElement to a DOM element.
+            Document document = this.getDocumentBuilder().newDocument();
+            Marshaller marshaller = this.getJAXBContext(type).createMarshaller();
+            marshaller.marshal(jaxbElement, document);
+            Element element = document.getDocumentElement();
+
+            return element;
+        }
+
+        @Override
+        public ExtensionAttribute unmarshal(final Element element) throws Exception {
+            if (null == element) {
+                return null;
+            }
+            // TODO look up type of extension attribute in CIMI resource
+            // metadata
+            Class<?> type = this.classLoader.loadClass("java.lang.String");
+
+            DOMSource source = new DOMSource(element);
+            Unmarshaller unmarshaller = this.getJAXBContext(type).createUnmarshaller();
+            JAXBElement<?> jaxbElement = unmarshaller.unmarshal(source, type);
+
+            // 3. Build the instance of ExtensionAttribute
+            ExtensionAttribute attribute = new ExtensionAttribute();
+            attribute.name = element.getLocalName();
+            attribute.value = jaxbElement.getValue();
+            return attribute;
+        }
+
     }
 
 }
