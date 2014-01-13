7bbc8afd4b7b603a32108e62acc0cd96ca2d7a5f
Frederic Dang Tran <frederic.dangtran@orange.com>
2014-01-13 17:03:50 +0100

    Fix retrieval of network list

--- sirocco-cimi-api-server-impl/src/main/java/org/ow2/sirocco/cimi/server/manager/network/CimiManagerReadNetworkCollection.java
+++ sirocco-cimi-api-server-impl/src/main/java/org/ow2/sirocco/cimi/server/manager/network/CimiManagerReadNetworkCollection.java
@@ -52,7 +52,7 @@ public class CimiManagerReadNetworkCollection extends CimiManagerReadAbstract {
     protected Object callService(final CimiContext context, final Object dataService) throws Exception {
         Object out = null;
         if (false == context.hasParamsForReadingCollection()) {
-            out = this.manager.getNetworks();
+            out = this.manager.getNetworks().getItems();
         } else {
             QueryResult<?> results = this.manager.getNetworks(context.valueOfFirst(), context.valueOfLast(),
                 context.valuesOfFilter(), context.valuesOfSelect());
